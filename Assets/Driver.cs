553dd92f33ad1b2b7263731e553c08ab7ae75656
Peter Weiser <peterweiser87@gmail.com>
2023-04-25 22:46:52 +0300

    added collision

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 0.2f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 300;
+    [SerializeField] float moveSpeed = 15;
 
     void Start()
     {
@@ -14,8 +14,9 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal");
-        transform.Rotate(0, 0, steerAmount);
-        transform.Translate(0, moveSpeed, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
