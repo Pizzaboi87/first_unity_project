b67c8e4d223dc6a07c8b05ebd67006621454687f
Peter Weiser <peterweiser87@gmail.com>
2023-04-27 21:33:12 +0300

    added sprites and draw map

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("What was that?!");
     }
 }
