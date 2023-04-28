77042b94b0b685440cc2d0b2208d51f3c9f8a81d
Peter Weiser <peterweiser87@gmail.com>
2023-04-28 20:54:02 +0300

    added spriterenderer

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 noPackageColor = new Color32(45, 121, 245, 255);
+    [SerializeField] Color32 hasPackageColor = new Color32(52, 200, 75, 255);
+
+    bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = noPackageColor;
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
@@ -11,13 +24,23 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up.");
+            hasPackage = true;
+            Destroy(other.gameObject);
+            spriteRenderer.color = hasPackageColor;
         }
-        else if (other.tag == "Customer")
+        else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered.");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+
+        else if (other.tag == "Customer" && !hasPackage)
+        {
+            Debug.Log("Hey, where is my package?!");
         }
     }
 }
